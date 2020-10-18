6c03ddfd290f79d794f50e2b51d2ada8883d3e49
Keagan Talbot <keaganpt@gmail.com>
2020-10-18 18:43:30 -0400

    modules completed

--- Module_1-5_Dom_&_API/module-3-JavaScript-API-Fundamentals/4-Apps/01-New-York-Times/nyt.js
+++ Module_1-5_Dom_&_API/module-3-JavaScript-API-Fundamentals/4-Apps/01-New-York-Times/nyt.js
@@ -19,16 +19,18 @@ const section = document.querySelector('section');
 
 nav.style.display = 'none';
 let pageNumber = 0;
+console.log('PageNumber:', pageNumber);
 let displayNav = false;
 
 searchForm.addEventListener('submit', fetchResults);
-nextBtn.addEventListener('click', nextPage);
 previousBtn.addEventListener('click', previousPage);
+nextBtn.addEventListener('click', nextPage);
 
 function fetchResults(e){
     e.preventDefault();
     //Assemble the full URL
     url = baseURL + '?api-key=' + key + '&page=' + pageNumber + '&q=' + searchTerm.value; 
+    console.log("URL:", url);
 
     if(startDate.value !== ''){
         console.log(startDate.value)
@@ -48,14 +50,73 @@ function fetchResults(e){
 }
 
 function displayResults(json) {
+    while (section.firstChild) {
+        section.removeChild(section.firstChild);
+    }
+
     let articles = json.response.docs;
-    console.log(articles);
+    
+    if(articles.length === 10){
+        nav.style.display = 'block'; //shws the nav display if 10 items are in the array
+    } else {
+        nav.style.display = 'none'; // hides the nav display if less than 10 items are in the array
+    }
+
+    if(articles.length === 0) {
+        console.log("No results");
+    } else {
+       for(let i = 0; i < articles.length; i++) {
+           let article = document.createElement('article');
+           let heading = document.createElement('h2');
+           let link = document.createElement('a');
+           let img = document.createElement('img');
+           let para = document.createElement('p');
+           let clearfix = document.createElement('div');
+
+           let current = articles[i];
+           console.log("Current:", current);
+
+           link.href = current.web_url;
+           link.textContent = current.headline.main;
+
+           para.textContent = 'Keywords: ';
+
+            for(let j =0; j < current.keywords.length; j++){
+                let span = document.createElement('span');
+                span.textContent += current.keywords[j].value + ' ';
+                para.appendChild(span);
+            }
+
+            if(current.multimedia.length > 0) {
+                img.src = 'http://www.nytimes.com/' + current.multimedia[0].url;
+                img.alt = current.headline.main;
+            }
+
+            clearfix.setAttribute('class','clearfix');
+            
+           article.appendChild(heading);
+           heading.appendChild(link);
+           article.appendChild(img);
+           article.appendChild(para);
+           article.appendChild(clearfix);
+           section.appendChild(article);
+
+       }
+    }
 };
 
-function nextPage(){
-    console.log("Next button clicked");
+function nextPage(e){
+    pageNumber++;
+    fetchResults(e);
+    console.log("Page Number:", pageNumber);
 }
 
-function previousPage(){
-    console.log("previous button clicked");
+function previousPage(e){
+    if(pageNumber > 0) {
+        pageNumber--;
+    } else {
+        return;
+    }
+    fetchResults(e);
+    console.log("Page:", pageNumber);
 }
